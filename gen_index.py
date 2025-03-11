ec70b21aa104452d9c6bf1ce0ef6bf5f758ffa4d
MyskYko <yoyuohlhjl@yahoo.co.jp>
2025-03-11 01:26:46 -0700

    gen_index

---
+++ gen_index.py
@@ -0,0 +1,33 @@
+import os
+import json
+
+def gen_index(path = 'benchmarks'):
+    data = {}
+    data['name'] = os.path.basename(path)
+    data['files'] = []
+    data['dirs'] = []
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.name.startswith('.'):
+                continue
+            if entry.path in gen_index.exclude:
+                continue
+            if entry.is_dir():
+                data['dirs'].append(gen_index(entry.path))
+            elif entry.name.endswith(gen_index.extension):
+                data['files'].append(entry.name)
+    return data
+gen_index.extension = ''
+gen_index.exclude = set()
+
+if __name__ == '__main__':
+    with open('settings.json', 'r') as f:
+        settings = json.load(f)
+        gen_index.extension = settings['extension']
+        for exclude in settings['benchmarks']['exclude']:
+            gen_index.exclude.add(os.path.normpath(os.path.join('benchmarks', exclude)))
+    data = gen_index()
+    json_object = json.dumps(data, indent=1)
+    with open('index.json', 'w') as f:
+        f.write(json_object)
+
