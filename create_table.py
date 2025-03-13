8fec21582c32021accd08053a37875442764c8aa
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2025-03-12 17:51:11 -0700

    table

--- create_table.py
+++ create_table.py
@@ -14,6 +14,8 @@ def create_table(path = 'results'):
                     df_list.append(df)
                     names.append(entry.name)
     df = pandas.concat(df_list, axis=1, keys=names).swaplevel(0, 1, axis=1).sort_index(axis=1)
+    if df.isna().any().any():
+        df = df.sort_index()
     if create_table.use_name:
         index_list = df.index.to_list()
         index_list = [os.path.split(index)[1] for index in index_list]
@@ -23,12 +25,18 @@ def create_table(path = 'results'):
         gmeans = {}
         for col in df.columns:
             gmeans[col] = numpy.exp(numpy.log(df[col]).mean())
-        gmean_df = pandas.DataFrame([gmeans], index=['gmean']).round(2).map(lambda x: '{0:.2f}'.format(x))
+        gmean_df = pandas.DataFrame([gmeans], index=['gmean']).map(lambda x: '{0:.2f}'.format(x))
         df = pandas.concat([df, gmean_df])
     pandas.set_option('display.max_rows', None)
     pandas.set_option('display.max_columns', None)
     pandas.set_option('display.max_colwidth', None)
-    print(df['size'])
+    pandas.set_option('display.width', None)
+    #print(df.columns.levels[0])
+    #print(df['size'])
+    #print(df['time'].astype(float).map(lambda x: '{0:.4f}'.format(x)))
+    df = pandas.concat([df['size'], df['time'].astype(float).map(lambda x: '{0:.4f}'.format(x))], keys=['size', 'time'])
+    print(df)
+    
 create_table.use_name = True
 create_table.add_gmean = True
 
